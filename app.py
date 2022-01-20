1739b424e38ad455c6f4d35966a7ea772263a2c8
JJneid <jjneid94@gmail.com>
2022-01-19 21:18:33 -0500

    error syntax fixed

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ server = app.server
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
 app.layout = html.Div([
-    html.H2('Hello Elsa),
+    html.H2('Hello Elsa'),
     dcc.Dropdown(
         id='dropdown',
         options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
