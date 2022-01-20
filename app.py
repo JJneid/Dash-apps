6972fa96ac66210871fbecafac1c0d0a9ec044f0
JJneid <jjneid94@gmail.com>
2022-01-19 21:30:12 -0500

    update with names dropdown

--- app.py
+++ app.py
@@ -9,14 +9,27 @@ server = app.server
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
 app.layout = html.Div([
-    html.H2('Hello Elsa'),
+    html.H2('Who are you??'),
+    html.Div([    dcc.Dropdown(
+            id='dropdown_name',
+            options=[{'label': i, 'value': i} for i in ['Elsa', 'Ralph', 'Maria']],
+            value='Maria'
+        )]),
+    html.H2(id='display-name-value'),
+    html.H2('Choose next carefully!!'),
     dcc.Dropdown(
         id='dropdown',
         options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
         value='LA'
     ),
-    html.Div(id='display-value')
+    html.H2(id='display-value')
 ])
+
+
+@app.callback(dash.dependencies.Output('display-name-value', 'children'),
+              [dash.dependencies.Input('dropdown_name', 'value')])
+def display_value(value):
+    return 'Hello "{}"'.format(value)
 
 @app.callback(dash.dependencies.Output('display-value', 'children'),
               [dash.dependencies.Input('dropdown', 'value')])
